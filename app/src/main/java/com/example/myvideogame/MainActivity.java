fc58c8c1f337b25f80024f73e6611044758e1d6c
AnaM339 <anamaria.manu39@gmail.com>
2024-03-04 11:10:22 +0100

    Setup

---
+++ app/src/main/java/com/example/myvideogame/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.myvideogame;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity{
+
+    @Override
+    public void onCreate(Bundle savedIstanceState) {
+        super.onCreate(savedIstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
